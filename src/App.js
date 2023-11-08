19aa38e19e5f0e9a4bd16490ec6ea0a72334c117
Karoly <karoly.webdev@gmail.com>
2023-11-08 09:04:57 +0000

    Make an n x m matrix, where n represents the num of columns, m represents the num of rows.

---
+++ src/App.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  /*
+    - Make an n x m matrix, where n represents the num of columns, m represents the num of rows.
+  */
+
+
+  function makeMatrix(n = 10, m = 10) {
+    let matrix = new Array(m).fill(new Array(n).fill(null));
+    matrix = matrix.map((y, yIndex) => y.map((x, xIndex) => x = { x: xIndex, y: yIndex, used: false }))
+    return matrix;
+  };
+
+  let matrix = makeMatrix();
+
+
+  return (
+    <div className="App">
+    </div>
+  );
+}
+
+export default App;
