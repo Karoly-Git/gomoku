fae54c88626f48fca82d09161370ecc03035ab87
Karoly <karoly.webdev@gmail.com>
2023-11-08 10:51:58 +0000

    Draw disks in different colors with every other click.

--- src/App.js
+++ src/App.js
@@ -8,12 +8,14 @@ function App() {
     - Get cell position when clicking on a cell.
     - When clicking on a cell, set the 'used' value to 'false' for the corresponding element in the matrix.
     - Draw a disk in the clicked cell.
+    - Draw disks in different colors with every other click. 
   */
 
+  const [playerA, setPlayerA] = useState(true);
 
   function makeMatrix(n = 10, m = 10) {
     let matrix = new Array(m).fill(new Array(n).fill(null));
-    matrix = matrix.map((y, yIndex) => y.map((x, xIndex) => x = { x: xIndex, y: yIndex, used: false }))
+    matrix = matrix.map((y, yIndex) => y.map((x, xIndex) => x = { x: xIndex, y: yIndex, used: false, player: '' }))
     return matrix;
   };
 
@@ -21,12 +23,14 @@ function App() {
 
   function handleClick(x, y) {
     console.log(x, y);
-    setUsedTrue(x, y);
+    setStatus(x, y);
+    setPlayerA(!playerA);
   }
 
-  function setUsedTrue(x, y) {
+  function setStatus(x, y) {
     let newMatrix = [...matrix];
     newMatrix[y][x].used = true;
+    newMatrix[y][x].player = playerA ? 'A' : 'B';
     setMatrix(newMatrix);
   }
 
@@ -39,7 +43,7 @@ function App() {
               {
                 row.map((cell, cIndex) =>
                   <div className='cell' key={cIndex} onClick={() => handleClick(cIndex, rIndex)}>
-                    {matrix[rIndex][cIndex].used ? <div className='disk'></div> : <></>}
+                    {matrix[rIndex][cIndex].used ? matrix[rIndex][cIndex].player === 'A' ? <div className='disk disk-A'></div> : <div className='disk disk-B'></div> : <></>}
                   </div>
                 )
               }
