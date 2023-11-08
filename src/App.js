3f46e59e14dbd703320e25a2a523570741b1fca3
Karoly <karoly.webdev@gmail.com>
2023-11-08 14:19:35 +0000

    Draw a disk in the clicked cell, but draw disks in different colors for the players, prevent overwriting the existing disks, set hover effect when cell is avaliable.

--- src/App.js
+++ src/App.js
@@ -3,34 +3,35 @@ import './App.css';
 
 function App() {
   /*
-    - Make an n x m matrix, where n represents the num of columns, m represents the num of rows.
-    - Draw a board based on the Matrix.
-    - Get cell position when clicking on a cell.
-    - When clicking on a cell, set the 'used' value to 'false' for the corresponding element in the matrix.
-    - Draw a disk in the clicked cell.
-    - Draw disks in different colors with every other click. 
+    1.  Make an n x m matrix, where n represents the num of columns, m represents the num of rows.
+    2.  Draw a board based on the Matrix.
+    3.  Get cell position when clicking on a cell.
+    4.  When clicking on a cell, set the 'player' value to the active player.
+    5.  Draw a disk in the clicked cell, 
+        but draw disks in different colors for the players, prevent overwriting the existing disks, 
+        set hover effect when cell is avaliable.
   */
 
-  const [playerA, setPlayerA] = useState(true);
+  const [activePlayer, setActivePlayer] = useState('A');
 
   function makeMatrix(n = 10, m = 10) {
     let matrix = new Array(m).fill(new Array(n).fill(null));
-    matrix = matrix.map((y, yIndex) => y.map((x, xIndex) => x = { x: xIndex, y: yIndex, used: false, player: '' }))
+    matrix = matrix.map((y, yIndex) => y.map((x, xIndex) => x = { x: xIndex, y: yIndex, player: '' }))
     return matrix;
   };
 
   const [matrix, setMatrix] = useState(makeMatrix());
 
   function handleClick(x, y) {
-    console.log(x, y);
-    setStatus(x, y);
-    setPlayerA(!playerA);
+    updateMatrix(x, y);
   }
 
-  function setStatus(x, y) {
+  function updateMatrix(x, y) {
     let newMatrix = [...matrix];
-    newMatrix[y][x].used = true;
-    newMatrix[y][x].player = playerA ? 'A' : 'B';
+    if (newMatrix[y][x].player === '') {
+      newMatrix[y][x].player = activePlayer;
+      setActivePlayer(newMatrix[y][x].player === 'A' ? 'B' : 'A');
+    }
     setMatrix(newMatrix);
   }
 
@@ -42,8 +43,8 @@ function App() {
             <div className='row' key={rIndex}>
               {
                 row.map((cell, cIndex) =>
-                  <div className='cell' key={cIndex} onClick={() => handleClick(cIndex, rIndex)}>
-                    {matrix[rIndex][cIndex].used ? matrix[rIndex][cIndex].player === 'A' ? <div className='disk disk-A'></div> : <div className='disk disk-B'></div> : <></>}
+                  <div className='cell' key={cIndex} onClick={() => handleClick(cIndex, rIndex)} style={matrix[rIndex][cIndex].player ? { backgroundColor: 'unset' } : {}}>
+                    {matrix[rIndex][cIndex].player === '' ? <></> : matrix[rIndex][cIndex].player === 'A' ? <div className='disk disk-A'></div> : <div className='disk disk-B'></div>}
                   </div>
                 )
               }
